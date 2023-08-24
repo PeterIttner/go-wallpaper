46ce9f9d1b522828184661c1af57a67a62ed2bc9
Peter Ittner <peter.ittner@conplement.de>
2023-08-24 11:05:34 +0200

    feat: add version information

--- cmd/wp/main.go
+++ cmd/wp/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"go-wp/internal/program"
 )
 
+var (
+	version = "dev"
+	date    = "unknown"
+)
+
 func main() {
+	fmt.Printf("running wp %s, built at %s\n", version, date)
 	program.Run()
 }
