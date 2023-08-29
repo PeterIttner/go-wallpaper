53b96a2536d08be026cbe73e3a8ccc01b2c60eba
Peter Ittner <peter.ittner@conplement.de>
2023-08-29 09:18:40 +0200

    feat: adds cobra for additional command line arguments

--- cmd/wp/main.go
+++ cmd/wp/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"github.com/spf13/cobra"
 	"go-wp/internal/program"
+	"os"
 )
 
 var (
@@ -10,7 +12,39 @@ var (
 	date    = "unknown"
 )
 
+var rootCmd = &cobra.Command{
+	Use:   "wp",
+	Short: "wp is a commandline tool for adjusting your wallpaper",
+	Long: `A commandline tools for applying a wallpaper
+                either from a web api.
+                or from a local directory
+                can also print text (watchwords) onto the wallpapers.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		program.Run()
+	},
+}
+
+func Init() {
+	rootCmd.AddCommand(versionCmd)
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of wp",
+	Long:  `All software has versions. This is wp's`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("running wp at version: \"%s\", built at %s\n", version, date)
+	},
+}
+
+func ExecuteRoot() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
 func main() {
-	fmt.Printf("running wp %s, built at %s\n", version, date)
-	program.Run()
+	Init()
+	ExecuteRoot()
 }
--- go.mod
+++ go.mod
@@ -7,12 +7,18 @@ require (
 	github.com/fogleman/gg v1.3.0
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
 	github.com/reujab/wallpaper v0.0.0-20210630195606-5f9f655b3740
+	github.com/spf13/cobra v1.7.0
 )
 
 require (
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stretchr/testify v1.8.3 // indirect
 	golang.org/x/image v0.11.0 // indirect
-	golang.org/x/sys v0.5.0 // indirect
-	gopkg.in/ini.v1 v1.62.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
